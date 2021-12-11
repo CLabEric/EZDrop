6509002c58c7048fb8302926c816ec1aaa1c73dc
EricNY <ericabt@gmail.com>
2021-12-11 15:24:59 -0500

    remove cookie object altogether

--- backend/app.js
+++ backend/app.js
@@ -42,11 +42,12 @@ app.use(session({
     saveUninitialized: false,
     // proxy: true,
     store: sessionStore,
-    cookie: {
+    // cookie: {
+        // sameSite: true,
         // sameSite:'none',
         // secure: true,
-        maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
-    }
+        // maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+    // }
 }));
 
 /**
