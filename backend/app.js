24e83a43aaa9c630b0c8760deea5598265045c1f
EricNY <ericabt@gmail.com>
2022-01-07 22:54:32 -0500

    trying to fix cookie issue.

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,15 @@ app.use(cors({
     origin: process.env.ORIGIN || "http://localhost:3001",
     credentials: true
 }));
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
+
 app.use(fileUpload());
 
 /**
